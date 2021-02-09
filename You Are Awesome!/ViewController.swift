d41f1c9ce15c1827e1998a5b96dbb3e2a3c663ad
Glitch (hello-webpage) <none>
2021-02-09 15:55:09 -0500

    2.9 conditional code (commented out)

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -33,34 +33,54 @@ class ViewController: UIViewController {
         imageView.image = UIImage (named: "image\(Int.random(in:0...totalNumberOfImages))")
         
         
-       /* messageLabel.text = messages [messageNumber]
-        messageNumber+=1
-        if messageNumber == messages.count {
-            messageNumber = 0
-        }*/
+        /* messageLabel.text = messages [messageNumber]
+         messageNumber+=1
+         if messageNumber == messages.count {
+         messageNumber = 0
+         }*/
         
-       // print (imageNumber)
-       // let imageName = "image" + String (imageNumber)
+        // print (imageNumber)
+        // let imageName = "image" + String (imageNumber)
         
-      /*  let imageName = "image\(imageNumber)"
-        imageView.image = UIImage ( named: imageName)
-        imageNumber = imageNumber + 1
-        if imageNumber  == 10 {
-            imageNumber = 0
-        }
-        
-        if  messageLabel.text == "You Are Awesome!" {
-            messageLabel.text = "You Are Great!"
-            imageView.image = UIImage(named: "image1")
-            
-        } else if  messageLabel.text == awesomeMessage {
-            
-        } else {
-            messageLabel.text = "You Are Awesome!"
-            imageView.image = UIImage(named: "image0")
-        }
- 
- */
+        /*  let imageName = "image\(imageNumber)"
+         imageView.image = UIImage ( named: imageName)
+         imageNumber = imageNumber + 1
+         if imageNumber  == 10 {
+         imageNumber = 0
+         }
+         
+         if  messageLabel.text == "You Are Awesome!" {
+         messageLabel.text = "You Are Great!"
+         imageView.image = UIImage(named: "image1")
+         
+         } else if  messageLabel.text == awesomeMessage {
+         
+         } else {
+         messageLabel.text = "You Are Awesome!"
+         imageView.image = UIImage(named: "image0")
+         }
+         
+         */
+        /*
+         
+         let awesomeMessage = "You Are Awesome!"
+         let greatMessage = "You Are Great!"
+         let bombMessage = "You Are Da Bomb!"
+         
+         if messageLabel.text == awesomeMessage {
+             messageLabel.text = greatMessage
+             imageView.image = UIImage (named: "image1")
+         } else if messageLabel.text == greatMessage {
+             messageLabel.text = bombMessage
+             imageView.image = UIImage (named: "image2")
+         } else {
+             messageLabel.text =  awesomeMessage
+             imageView.image = UIImage (named: "image0")
+         }
+         
+         
+         
+         */
         
     }
     
